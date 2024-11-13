af863d7a22d15a1260bfc12890404c5e68361dbc
dothaofpt <143236377+dothaofpt@users.noreply.github.com>
2024-11-13 15:13:27 +0700

    up

--- serviceUser/src/main/java/org/example/serviceuser/userService/jwt/JwtUtil.java
+++ serviceUser/src/main/java/org/example/serviceuser/userService/jwt/JwtUtil.java
@@ -3,15 +3,18 @@ package org.example.serviceuser.userService.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class JwtUtil {
-    private String SECRET_KEY = "secret";
+    // Khóa mạnh được tạo từ lớp Keys
+    private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateToken(String username) {
         Map<String, Object> claims = new HashMap<>();
@@ -24,10 +27,9 @@ public class JwtUtil {
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // Hết hạn sau 10 giờ
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SECRET_KEY) // Sử dụng khóa mạnh
                 .compact();
     }
-
 
     public Boolean validateToken(String token, String username) {
         final String extractedUsername = extractUsername(token);
--- serviceproduct/src/main/java/org/example/productservice/ServiceproductApplication.java
+++ serviceproduct/src/main/java/org/example/productservice/ServiceproductApplication.java
@@ -2,8 +2,10 @@ package org.example.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceproductApplication {
 
     public static void main(String[] args) {
